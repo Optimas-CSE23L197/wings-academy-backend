a7f3397a414a5db5b3e236ca5ec1e71b12e5478d
Animesh <karananimesh144@gmail.com>
2025-11-16 10:48:32 +0530

    add gateway service

---
+++ elearning-gateway/elearning-gateway/src/main/java/com/elearning_gateway/elearning_gateway/ElearningGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.elearning_gateway.elearning_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElearningGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElearningGatewayApplication.class, args);
+	}
+
+}
---
+++ elearning-gateway/elearning-gateway/src/test/java/com/elearning_gateway/elearning_gateway/ElearningGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elearning_gateway.elearning_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElearningGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
